183975e278c1200c7a704a90f2833bc910c1a5a7
Sivapragasam <qc.siva@gmail.com>
2026-05-07 16:05:17 +0530

    Added JWT auth middleware and protected routes

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+  let token;
+
+  try {
+    // Check if token exists
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      // Get token from header
+      token = req.headers.authorization.split(" ")[1];
+
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      
+      // console.log("Process.env.secret is :", process.env.JWT_SECRET)
+
+      // Get user from DB (exclude password)
+      req.user = await User.findById(decoded.id).select("-password");
+
+      next();
+    } else {
+      return res.status(401).json({ message: "Not authorized, no token," });
+    }
+  } catch (error) {
+    return res.status(401).json({
+      message: "Token failed",
+    });
+  }
+};
+
+module.exports = protect;
--- server.js
+++ server.js
@@ -4,26 +4,35 @@ const express = require("express");
 const app = express();
 const User = require("./models/User");
 const authRoutes = require("./routes/authRoutes");
+const protect = require("./middleware/authMiddleware");
 
 //connect Database
 connectDB();
 
 //Middleware
 app.use(express.json());
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 //test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 //Test route for schema adding
-app.get("/test-user", async (req, res) => {
-  const user = await User.create({
-    name: "Test User",
-    email: "test@example.com",
-    password: "123456",
+// app.get("/test-user", async (req, res) => {
+//   const user = await User.create({
+//     name: "Test User",
+//     email: "test@example.com",
+//     password: "123456",
+//   });
+//   res.json(user);
+// });
+
+//Test Middle ware
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user,
   });
-  res.json(user);
 });
 
 //start server
